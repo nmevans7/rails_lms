c5ea62aa618df162cb873ce1b7409a257bdef707
natalie evans <nmevans7@gmail.com>
2020-06-03 20:54:01 -0600

    added models controllers views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+roles = ['teacher', 'ta', 'student']
+10.times do
+  course = Course.create(
+    name: Faker::Science.scientist
+  )
+  5.times do
+    user = User.create(
+      first_name: Faker::TvShows::RickAndMorty.character,
+      last_name: Faker::Games::Zelda.character
+    )
+
+  Enrollment.create(
+    role: roles.sample,
+    course_id: course.id,
+    user_id: user.id
+  )
+  end 
+end
+
+puts 'data seeded'
